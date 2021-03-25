2e39bf3085b1e4a30130be858ff806aeb3d44cf8
whats2000 <60466660+whats2000@users.noreply.github.com>
2021-03-26 00:15:49 +0800

    Update __main__.py

--- QA_discordBot/mainPython/__main__.py
+++ QA_discordBot/mainPython/__main__.py
@@ -3,60 +3,63 @@ from discord.ext import commands
 import googletrans
 from googletrans import Translator
 
-def translate_text(oringinlang, targetlang, input):
-	t = googletrans.Translator()
-	if oringinlang == None :
-		result = t.translate(
-		input, 
-		dest = targetlang
-		)
-	else :
-		result = t.translate(
-		input, 
-		scr = originlang, 
-		dest = targetlang
-		)
-	
-	print('目標語言 -> ' 
-	+ str(targetlang)
-	)
-	
-	print('句子 -> ' 
-	+ str(input)
-	+ ' >>> '
-	+ str(result.text)
-	)
-	
-	return result
 
-bot = commands.Bot(command_prefix=  'r?')
+def translate_text(originlang, targetlang, input):
+    t = googletrans.Translator()
+    if originlang is None:
+        result = t.translate(
+            input,
+            dest=targetlang
+        )
+    else:
+        result = t.translate(
+            input,
+            dest=originlang
+        )
+
+    print('目標語言 -> '
+          + str(targetlang)
+          )
+
+    print('句子 -> '
+          + str(input)
+          + ' >>> '
+          + str(result.text)
+          )
+
+    return result
+
+
+bot = commands.Bot(command_prefix='r?')
+
 
 @bot.event
 async def on_ready():
     print("debug code 0")
-    
+
+
 @bot.command()
 async def translate(ctx, lang, *, args):
-	print('位置 -> ' 
-	+ str(ctx)
-	)
-	
-	result =translate_text(None, lang, args)
+    print('位置 -> '
+          + str(ctx)
+          )
 
-	embed=discord.Embed(
-	title=result.text, 
-	description=result.src
-		+' -> '
-		+ result.dest, 
-	color=0x00ff00
-	)
+    result = translate_text(None, lang, args)
 
-	embed.set_author(
-	name = ctx.message.author.name, 
-	icon_url = ctx.message.author.avatar_url
-	)
+    embed = discord.Embed(
+        title=result.text,
+        description=result.src
+                    + ' -> '
+                    + result.dest,
+        color=0x00ff00
+    )
 
-	await ctx.send(embed=embed)
+    embed.set_author(
+        name=ctx.message.author.name,
+        icon_url=ctx.message.author.avatar_url
+    )
 
-bot.run('token')
+    await ctx.send(embed=embed)
 
+
+bot.run("token")
