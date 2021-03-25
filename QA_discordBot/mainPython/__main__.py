2e89bdbb71f44698a5c7d20bd3926cacdf62530c
whats2000 <60466660+whats2000@users.noreply.github.com>
2021-03-25 18:03:48 +0800

    Main : Add translations features

--- QA_discordBot/mainPython/__main__.py
+++ QA_discordBot/mainPython/__main__.py
@@ -1,14 +1,50 @@
 import discord
 from discord.ext import commands
+import googletrans
+from googletrans import Translator
 
-bot = commands.Bot(command_prefix='r?')
+def translate_text(oringinlang, targetlang, input):
+	t = googletrans.Translator()
+	if oringinlang == None :
+		result = t.translate(input, dest = targetlang )
+	else :
+		result = t.translate(input, scr = originlang, dest = targetlang )
+	
+	print('目標語言 -> ' + str(targetlang))
+	
+	print('句子 -> ' + str(input)
+	+ ' >>> '
+	+ str(result.text))
+	
+	return result
+
+bot = commands.Bot(command_prefix=  'r?')
 
 @bot.event
 async def on_ready():
     print("debug code 0")
+    
+@bot.command()
+async def translate(ctx, lang, *, args):
+	print('位置 -> ' + str(ctx))
+	#print('目標語言 -> ' + str(lang))
+	#print('句子 -> ' + str(args), end=' >>> ')
+	#t = googletrans.Translator()
+	result =translate_text(None, lang, args)
 
+	embed=discord.Embed(
+	title=result.text
+	, description=result.src
+		+' -> '
+		+ result.dest
+	, color=0x00ff00
+	)
 
+	embed.set_author(
+	name = ctx.message.author.name, icon_url = ctx.message.author.avatar_url
+	)
 
-bot.run('hide')
+	await ctx.send(embed=embed)
 
+bot.run('token')
 
