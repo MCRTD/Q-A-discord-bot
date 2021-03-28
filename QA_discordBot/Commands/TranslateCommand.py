978239759b47aba1e808fccec66539073460915c
whats2000 <60466660+whats2000@users.noreply.github.com>
2021-03-29 00:37:53 +0800

    Rename classes

--- QA_discordBot/Commands/TranslateCommand.py
+++ QA_discordBot/Commands/TranslateCommand.py
@@ -1,10 +1,10 @@
 import discord
 from discord.ext import commands
-from QA_discordBot.Core.Classes import Cod_Extension
-from QA_discordBot.resources import TranslateText
+from QA_discordBot.Core.Common import Cod_Extension
+from QA_discordBot.Core import TranslateText
 
 
-class Main(Cod_Extension):
+class TranslateCommand(Cod_Extension):
 
     @commands.command()
     async def translate(self, ctx, lang, *, args):
@@ -29,22 +29,6 @@ class Main(Cod_Extension):
 
         await ctx.send(embed=embed)
 
-    @commands.command()
-    async def ping(self, ctx):
-        print('呼叫延遲檢測 -> ' + str(self.bot.latency))
-        embed = discord.Embed(
-            title="連線延遲 ping",
-            description=str(self.bot.latency*1000)+' ms',
-            color=0x00ff00
-        )
-
-        embed.set_author(
-            name=ctx.message.author.name,
-            icon_url=ctx.message.author.avatar_url
-        )
-
-        await ctx.send(embed=embed)
-
 
 def setup(pybot):
-    pybot.add_cog(Main(pybot))
+    pybot.add_cog(TranslateCommand(pybot))
--- QA_discordBot/Core/Common.py
+++ QA_discordBot/Core/Common.py
@@ -5,3 +5,4 @@ from discord.ext import commands
 class Cod_Extension(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
